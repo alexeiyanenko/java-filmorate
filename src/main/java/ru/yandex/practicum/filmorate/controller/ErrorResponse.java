91331be5962d59d7e0d1c02153c656da5e835861
Alexei Yanenko <yanenkome@yandex.ru>
2024-10-08 16:51:28 +0300

    Выполнить ТЗ спринта 10 (без тестов)

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorResponse.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ErrorResponse {
+    private String message;
+
+    public ErrorResponse(String message) {
+        this.message = message;
+    }
+}
+
+
