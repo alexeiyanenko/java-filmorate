27a30c271ba1af2d803266d56f07950fcb320729
Alexei Yanenko <yanenkome@yandex.ru>
2025-01-04 01:37:31 +0300

    Исправить недочеты по стилю кода

--- src/main/java/ru/yandex/practicum/filmorate/storage/memoryImpl/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/memoryImpl/InMemoryFilmStorage.java
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Component;
 
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.Genre;
-import ru.yandex.practicum.filmorate.model.MPA;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
 
 import java.util.*;
