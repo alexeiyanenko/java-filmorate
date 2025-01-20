541a53f5fe30c28244c5858f38b1b440285ff008
Vasiliy <toropovforwork@mail.ru>
2025-01-20 21:46:12 +0300

    Исправление кода, рефакторинг. Исправление чекстайла.

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -19,7 +19,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 
 import jakarta.validation.Valid;
-import ru.yandex.practicum.filmorate.storage.EventStorage;
 
 import java.util.List;
 
